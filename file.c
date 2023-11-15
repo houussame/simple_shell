a5b961e473fe9631002b4d98bec4f812996edb49
houussame <2015.houdou@gmail.com>
2023-11-15 12:18:44 -0800

    file

--- file.c
+++ file.c
@@ -3,6 +3,9 @@
  * main - the main function
  * @sz: size
  * @ln: line
+ * @nRead: numRead
+ * @pd: pid
+ * @st: status
  * Return: always 0
  */
 int main(void)
@@ -10,25 +13,34 @@ int main(void)
 	char *ln = NULL;
 	char *args[] = {NULL, NULL};
 	size_t sz;
-	int numRead;
+	int nRead;
+	int pd;
+	int st;
 
 	while (1)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
-		numRead = getline(&ln, &sz, stdin);
-		if (numRead >= 0)
+		nRead = getline(&ln, &sz, stdin);
+		if (nRead >= 0)
 		{
-
+			ln[nRead - 1] = '\0';/*so we can remove the new line*/
+			pd = fork(); /*creat new proccess*/
 			args[0] = ln;
-			execve(ln, args, NULL);
+			if (pd == 0)
+				execve(ln, args, NULL);
+			else if (pd > 0)
+				wait(&st);
+			else if (pd < 0)
+				perror;
 			free(ln);   /*because you used getline*/
 			ln = NULL; /*always give line a NULL when you use free*/
 		}
 		else
 		{
-			free(ln);
+			free(ln); 
 			break;
 		}
 	}
 	return (0);
 }
+
--- main.h
+++ main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #endif
